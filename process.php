f19c4ee97f28b72594f76597b41da9385bb2fefb
poolpal <zoo5871@naver.com>
2020-08-03 13:59:31 +0900

    2020-08-03

--- process.php
+++ process.php
@@ -3,7 +3,10 @@
     require("lib/db.php");
     $conn=db_init($config["host"],$config["duser"],$config["dpw"],$config["dname"]);
 
-    $sql="Insert into topic(title,autor,description,created) values"."('".$_POST['title']."','".$_POST['autor']."','".$_POST['description']."',now())";
+    $title=mysqli_real_escape_string($conn,$_POST['title']);
+    $autor=mysqli_real_escape_string($conn,$_POST['autor']);
+    $desc=mysqli_real_escape_string($conn,$_POST['description']);
+    $sql="Insert into topic(`title`,`autor`,`description`,`created`) values"."('{$title}','{$autor}','{$desc}',now())";
 
     $result=mysqli_query($conn,$sql);
 
